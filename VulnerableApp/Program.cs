e5af4e6b42bc93504f11933f2bcf24c84aec88f1
jovanny67u7 <jovannyyy777@gmail.com>
2026-06-02 13:49:43 -0600

    avance practica

---
+++ VulnerableApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using VulnerableApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
